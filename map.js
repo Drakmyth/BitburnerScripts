be899f94e621f8d90cdd48c88c5659e6ecacdc29
Drakmyth <shaun.hamman@gmail.com>
2022-04-07 21:20:46 -0700

    Added --organization flag to map.js to show organization name

--- map.js
+++ map.js
@@ -1,9 +1,26 @@
 /** @param {NS} ns */
+function print_host(ns, prefix, host) {
+	let label = prefix + "  \\-- " + host;
+
+	let flags = ns.flags([
+		['o', false],
+		['organization', false]
+	]);
+
+	let show_organization = flags['o'] || flags['organization'];
+	if (show_organization) {
+		label += " (" + ns.getServer(host).organizationName + ")";
+	}
+
+	ns.tprint(label);
+}
+
+/** @param {NS} ns */
 function walk(ns, host, prefix="") {
 	let servers = ns.scan(host);
 	servers.shift();
 	for (let [index, next] of servers.entries()) {
-		ns.tprint(prefix + "  \\-- " + next);
+		print_host(ns, prefix, next);
 		let next_prefix = prefix + (index < servers.length - 1 ? "  |  " : "     ")
 		walk(ns, next, next_prefix);
 	}
