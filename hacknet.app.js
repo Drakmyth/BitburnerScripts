f64ef29d4324c58e298d43d93d420362899f0cfe
Drakmyth <shaun.hamman@gmail.com>
2022-04-13 13:10:19 -0700

    Disabled all default logging in hacknet.app.js

--- hacknet.app.js
+++ hacknet.app.js
@@ -10,11 +10,11 @@ class Upgrade {
 /** @param {NS} ns **/
 export async function main(ns) {
     ns.tail();
-    ns.disableLog("sleep");
+    ns.disableLog(`ALL`);
     while (true) {
         let maximumCost = ns.getPlayer().money * 0.25;
-        let purchaseUpgrade = new Upgrade("purchase", -1, ns.hacknet.getPurchaseNodeCost(), () => { ns.hacknet.purchaseNode() });
-        let bestUpgrade = new Upgrade("none", -1, 0, async () => { await ns.sleep(5000); });
+        let purchaseUpgrade = new Upgrade(`purchase`, -1, ns.hacknet.getPurchaseNodeCost(), () => { ns.hacknet.purchaseNode() });
+        let bestUpgrade = new Upgrade(`none`, -1, 0, async () => { await ns.sleep(5000); });
 
         let ownedNodes = ns.hacknet.numNodes();
         if (ownedNodes < 1 && purchaseUpgrade.cost < maximumCost) {
@@ -24,9 +24,9 @@ export async function main(ns) {
         for (let i = 0; i < ownedNodes; i++) {
             let upgrades = [
                 purchaseUpgrade,
-                new Upgrade("level", i, ns.hacknet.getLevelUpgradeCost(i, 1), () => ns.hacknet.upgradeLevel(i, 1)),
-                new Upgrade("ram", i, ns.hacknet.getRamUpgradeCost(i, 1), () => ns.hacknet.upgradeRam(i, 1)),
-                new Upgrade("cores", i, ns.hacknet.getCoreUpgradeCost(i, 1), () => ns.hacknet.upgradeCore(i, 1))
+                new Upgrade(`level`, i, ns.hacknet.getLevelUpgradeCost(i, 1), () => ns.hacknet.upgradeLevel(i, 1)),
+                new Upgrade(`ram`, i, ns.hacknet.getRamUpgradeCost(i, 1), () => ns.hacknet.upgradeRam(i, 1)),
+                new Upgrade(`cores`, i, ns.hacknet.getCoreUpgradeCost(i, 1), () => ns.hacknet.upgradeCore(i, 1))
             ];
 
             for (let upgrade of upgrades) {
@@ -37,10 +37,10 @@ export async function main(ns) {
         }
 
         await bestUpgrade.func();
-        if (bestUpgrade.type === "purchase") {
-            ns.print("Purchased node for " + ns.nFormat(bestUpgrade.cost, '($0.000a)') + ".");
-        } else if (bestUpgrade.type !== "none") {
-            ns.print("Upgraded hacknet-node-" + bestUpgrade.node + " " + bestUpgrade.type + " for " + ns.nFormat(bestUpgrade.cost, '($0.000a)') + ".");
+        if (bestUpgrade.type === `purchase`) {
+            ns.print(`Purchased node for ${ns.nFormat(bestUpgrade.cost, `($0.000a)`)}.`);
+        } else if (bestUpgrade.type !== `none`) {
+            ns.print(`Upgraded hacknet-node-${bestUpgrade.node} ${bestUpgrade.type} for ${ns.nFormat(bestUpgrade.cost, `($0.000a)`)}.`);
         }
     }
 }
