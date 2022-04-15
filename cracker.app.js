e5caa776392cbee86a277b934b9e90b13953cadd
Drakmyth <shaun.hamman@gmail.com>
2022-04-14 21:18:41 -0700

    Added cracker.app.js

---
+++ cracker.app.js
@@ -0,0 +1,53 @@
+class Program {
+    constructor(filename, execFunc) {
+        this.filename = filename;
+        this.execute = execFunc;
+    }
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.tail();
+    ns.disableLog(`ALL`);
+    let tenMinutes = 1000 * 60 * 10;
+    let serverFile = `known-servers.json`;
+    let programs = [
+        new Program(`BruteSSH.exe`, host => ns.brutessh(host)),
+        new Program(`SQLInject.exe`, host => ns.sqlinject(host)),
+        new Program(`relaySMTP.exe`, host => ns.relaysmtp(host)),
+        new Program(`FTPCrack.exe`, host => ns.ftpcrack(host)),
+        new Program(`HTTPWorm.exe`, host =>ns.httpworm(host))
+    ]
+    while(true) {
+        let servers = JSON.parse(ns.read(serverFile));
+        ns.print(`Reloaded ${serverFile}`);
+        let playerSkill = ns.getHackingLevel();
+        let ownedPrograms = programs.filter(p => ns.fileExists(p.filename));
+
+        let crackedAny = false;
+        for (let server of servers) {
+            if (server.hasAdminRights) continue;
+            if (server.requiredHackingSkill > playerSkill) continue;
+            if (server.numOpenPortsRequired > ownedPrograms.length) continue;
+
+            ns.print(`\nCracking ${server.hostname}...`);
+            ns.print(`Skill: ${playerSkill}/${server.requiredHackingSkill} Ports: ${server.openPortCount}/${server.numOpenPortsRequired}`);
+            ns.print(`Opening ports...`)
+            for (let program of ownedPrograms) {
+                program.execute(server.hostname);
+            }
+            
+            ns.print(`Nuking...`)
+            ns.nuke(server.hostname);
+            ns.print(`${server.hostname} cracked.`)
+            crackedAny = true;
+        }
+        
+        if (!crackedAny) {
+            ns.print(`No known crackable servers.`);
+        }
+
+        ns.print(`Will search again in 10 minutes.`);
+        await ns.sleep(tenMinutes);
+    }
+}
