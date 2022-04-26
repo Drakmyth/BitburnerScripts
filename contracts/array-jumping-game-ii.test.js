eb22cd36277e4fb9fea5d299713918d00fca80fe
Drakmyth <shaun.hamman@gmail.com>
2022-04-25 18:16:48 -0700

    Updated credit on Array Jumping Game II test cases

--- contracts/array-jumping-game-ii.test.js
+++ contracts/array-jumping-game-ii.test.js
@@ -10,6 +10,7 @@ class TestCase {
 /** @param {NS} ns */
 export async function main(ns) {
     const testCases = [
+        // Test cases are real contracts I've solved
         new TestCase([2, 0, 0, 1, 3, 0, 0, 3, 2, 5, 5, 3, 2, 0, 0, 6, 3, 0, 3, 2, 5, 1, 1, 3], 0),
         new TestCase([5, 3, 6, 2, 4, 6, 1, 7, 1, 3, 1, 5, 1, 4, 1, 2, 0], 3)
     ];
