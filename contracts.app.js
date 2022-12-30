9927e266f74df7c5de6a889d033a053b32f75d96
Drakmyth <shaun.hamman@gmail.com>
2022-12-29 22:34:32 -0800

    Refactor contract solver execution logic and pull into library script

--- contracts.app.js
+++ contracts.app.js
@@ -1,90 +1,37 @@
-import * as Ports from "ports.lib.js";
-
-class ContractType {
-    constructor(title, script = ``, inputProcessor = (input) => input) {
-        this.title = title;
-        this.script = script;
-        this.processInput = inputProcessor;
-    }
-}
-
-const contractTypes = [
-    new ContractType(`Algorithmic Stock Trader I`, `contracts/algorithmic-stock-trader.js`, (input) => [1, input]),
-    new ContractType(`Algorithmic Stock Trader II`, `contracts/algorithmic-stock-trader.js`, (input) => [input.length, input]),
-    new ContractType(`Algorithmic Stock Trader III`, `contracts/algorithmic-stock-trader.js`, (input) => [2, input]),
-    new ContractType(`Algorithmic Stock Trader IV`, `contracts/algorithmic-stock-trader.js`),
-    new ContractType(`Array Jumping Game`, `contracts/array-jumping-game.js`),
-    new ContractType(`Array Jumping Game II`, `contracts/array-jumping-game-ii.js`),
-    new ContractType(`Compression I: RLE Compression`, `contracts/compression-i-rle-compression.js`),
-    new ContractType(`Compression II: LZ Decompression`, `contracts/compression-ii-lz-decompression.js`),
-    new ContractType(`Encryption I: Caesar Cipher`, `contracts/encryption-i-caesar-cipher.js`),
-    new ContractType(`Encryption II: Vigenère Cipher`, `contracts/encryption-ii-vigenere-cipher.js`),
-    new ContractType(`Find Largest Prime Factor`, `contracts/find-largest-prime-factor.js`),
-    new ContractType(`Generate IP Addresses`, `contracts/generate-ip-addresses.js`),
-    new ContractType(`HammingCodes: Encoded Binary to Integer`, `contracts/hammingcodes-encoded-binary-to-integer.js`),
-    new ContractType(`HammingCodes: Integer to Encoded Binary`, `contracts/hammingcodes-integer-to-encoded-binary.js`),
-    new ContractType(`Merge Overlapping Intervals`, `contracts/merge-overlapping-intervals.js`),
-    new ContractType(`Minimum Path Sum in a Triangle`, `contracts/minimum-path-sum-in-a-triangle.js`),
-    new ContractType(`Proper 2-Coloring of a Graph`, `contracts/proper-2-coloring-of-a-graph.js`),
-    new ContractType(`Sanitize Parentheses in Expression`, `contracts/sanitize-parentheses-in-expression.js`),
-    new ContractType(`Shortest Path in a Grid`, `contracts/shortest-path-in-a-grid.js`),
-    new ContractType(`Spiralize Matrix`, `contracts/spiralize-matrix.js`),
-    new ContractType(`Subarray with Maximum Sum`, `contracts/subarray-with-maximum-sum.js`),
-    new ContractType(`Total Ways to Sum`, `contracts/total-ways-to-sum.js`, (input) => [input, [...Array(input).keys()].filter(a => a > 0)]),
-    new ContractType(`Total Ways to Sum II`, `contracts/total-ways-to-sum.js`),
-    new ContractType(`Unique Paths in a Grid I`, `contracts/unique-paths-in-a-grid.js`, (input) => Array(input[0]).fill(null).map(() => Array(input[1]).fill(0))),
-    new ContractType(`Unique Paths in a Grid II`, `contracts/unique-paths-in-a-grid.js`)
-];
+import { ContractSolver } from "contracts.lib.js";
 
 class Contract {
-    constructor(type, input, host, file) {
-        this.type = type;
-        this.input = input;
+    constructor(title, filename, host) {
+        this.title = title;
+        this.filename = filename;
         this.host = host;
-        this.file = file;
-        this.reward = ``;
-        this.failure = ``;
-    }
-
-    toString() {
-        return `${this.host} - ${this.file} - ${this.type.title}`;
-    }
-
-    /** @param {NS} ns */
-    async solve(ns) {
-        if (this.type.script === ``) {
-            return `new`;
-        }
-
-        const processedInput = this.type.processInput(this.input);
-        ns.run(this.type.script, 1, JSON.stringify(processedInput), Ports.CONTRACT_PORT);
-
-        const port = ns.getPortHandle(Ports.CONTRACT_PORT);
-        while (port.empty()) {
-            await ns.sleep(1);
-        }
-
-        const answer = JSON.parse(port.read());
-        const result = ns.codingcontract.attempt(answer, this.file, this.host, { returnReward: true });
-        if (result !== ``) {
-            this.reward = result;
-            return `reward`;
-        } else {
-            this.failure = `Answer: ${answer}, Input: ${this.input}`;
-            return `fail`
-        }
     }
 }
 
 /** @param {NS} ns */
-function getContracts(ns, host) {
+function getContractsFromHost(ns, host) {
     const contracts = [];
-    const contractFiles = ns.ls(host, `.cct`);
-    for (let file of contractFiles) {
-        const title = ns.codingcontract.getContractType(file, host);
-        const type = contractTypes.find(t => t.title === title) || new ContractType(title);
-        const input = ns.codingcontract.getData(file, host);
-        contracts.push(new Contract(type, input, host, file));
+    const contractFilenames = ns.ls(host, `.cct`);
+    for (let filename of contractFilenames) {
+        const title = ns.codingcontract.getContractType(filename, host);
+        contracts.push(new Contract(title, filename, host));
+    }
+
+    return contracts;
+}
+
+/** @param {NS} ns */
+function findAllContracts(ns) {
+    const serverFile = `known-servers.json.txt`;
+    const servers = JSON.parse(ns.read(serverFile));
+
+    ns.print(`\nReloaded ${serverFile}`);
+    ns.print(`Searching for contracts...`);
+
+    const contracts = [];
+    for (let server of servers) {
+        const hostContracts = getContractsFromHost(ns, server.hostname);
+        contracts.push(...hostContracts);
     }
 
     return contracts;
@@ -93,42 +40,34 @@ function getContracts(ns, host) {
 /** @param {NS} ns */
 export async function main(ns) {
     ns.disableLog(`ALL`);
-    ns.tail();
 
     const tenMinutes = 1000 * 60 * 10;
-    const serverFile = `known-servers.json.txt`;
+
     while (true) {
-        const servers = JSON.parse(ns.read(serverFile));
+        const contracts = findAllContracts(ns);
 
-        ns.print(`\nReloaded ${serverFile}`);
-        ns.print(`Searching for contracts...`);
-
-        let foundContracts = false;
-        for (let server of servers) {
-            const contracts = getContracts(ns, server.hostname);
-            foundContracts = foundContracts || contracts.length > 0;
-            for (let contract of contracts) {
-                ns.print(`Found: ${contract}`);
-                const status = await contract.solve(ns);
-                switch (status) {
-                    case `reward`:
-                        ns.print(`    Reward: ${contract.reward}`);
-                        break;
-                    case `fail`:
-                        ns.print(`    !!!! FAILED !!!!`);
-                        ns.print(`    Failure: ${contract.failure}`);
-                        break;
-                    case `new`:
-                        ns.print(`    !!!! NEW !!!!`);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        if (!foundContracts) {
+        if (contracts.length < 1) {
             ns.print(`No contracts found.`);
         }
+
+        for (let contract of contracts) {
+            ns.print(`Found: ${contract.host} - ${contract.filename} - ${contract.title}`);
+            const solver = ContractSolver.findSolver(contract.title);
+            if (solver === undefined) {
+                ns.print(`    !!!! NEW !!!!`);
+                continue;
+            }
+
+            const result = await solver.solve(ns, contract.filename, contract.host);
+            let prefix = `Reward`;
+            if (!result.solved) {
+                ns.tail();
+                ns.print(`    !!!! FAILED !!!!`);
+                prefix = `Failure`;
+            }
+            ns.print(`    ${prefix}: ${result.message}`);
+        }
+
         ns.print(`Will search again at ${new Date(Date.now() + tenMinutes).toLocaleTimeString(_, { hour12: false })}.`);
         await ns.sleep(tenMinutes);
     }
---
+++ contracts.lib.js
@@ -0,0 +1,106 @@
+import * as Ports from "ports.lib.js";
+
+// generated from ns.codingcontracts.getContractTypes()
+export const ContractTypes = {
+    FIND_LARGEST_PRIME_FACTOR: `Find Largest Prime Factor`,
+    SUBARRAY_WITH_MAXIMUM_SUM: `Subarray with Maximum Sum`,
+    TOTAL_WAYS_TO_SUM_1: `Total Ways to Sum`,
+    TOTAL_WAYS_TO_SUM_2: `Total Ways to Sum II`,
+    SPIRALIZE_MATRIX: `Spiralize Matrix`,
+    ARRAY_JUMPING_GAME_1: `Array Jumping Game`,
+    ARRAY_JUMPING_GAME_2: `Array Jumping Game II`,
+    MERGE_OVERLAPPING_INTERVALS: `Merge Overlapping Intervals`,
+    GENERATE_IP_ADDRESSES: `Generate IP Addresses`,
+    ALGORITHMIC_STOCK_TRADER_1: `Algorithmic Stock Trader I`,
+    ALGORITHMIC_STOCK_TRADER_2: `Algorithmic Stock Trader II`,
+    ALGORITHMIC_STOCK_TRADER_3: `Algorithmic Stock Trader III`,
+    ALGORITHMIC_STOCK_TRADER_4: `Algorithmic Stock Trader IV`,
+    MINIMUM_PATH_SUM_IN_A_TRIANGLE: `Minimum Path Sum in a Triangle`,
+    UNIQUE_PATHS_IN_A_GRID_1: `Unique Paths in a Grid I`,
+    UNIQUE_PATHS_IN_A_GRID_2: `Unique Paths in a Grid II`,
+    SHORTEST_PATH_IN_A_GRID: `Shortest Path in a Grid`,
+    SANITIZE_PARENTHESES_IN_EXPRESSION: `Sanitize Parentheses in Expression`,
+    FIND_ALL_VALID_MATH_EXPRESSIONS: `Find All Valid Math Expressions`,
+    HAMMINGCODES_ENCODE: `HammingCodes: Integer to Encoded Binary`,
+    HAMMINGCODES_DECODE: `HammingCodes: Encoded Binary to Integer`,
+    PROPER_2_COLORING_OF_A_GRAPH: `Proper 2-Coloring of a Graph`,
+    COMPRESSION_1: `Compression I: RLE Compression`,
+    COMPRESSION_2: `Compression II: LZ Decompression`,
+    COMPRESSION_3: `Compression III: LZ Compression`,
+    ENCRYPTION_1: `Encryption I: Caesar Cipher`,
+    ENCRYPTION_2: `Encryption II: Vigenère Cipher`
+};
+
+class SolveResult {
+    constructor(reward, failure) {
+        this.solved = failure === '';
+        this.message = this.solved ? reward : failure;
+    }
+
+    static success = (message) => new SolveResult(message, '');
+    static failure = (message) => new SolveResult('', message);
+}
+
+export class ContractSolver {
+    constructor(title, script, inputProcessor = (input) => input) {
+        this.title = title;
+        this.script = script;
+        this.processInput = inputProcessor;
+    }
+
+    /** @param {NS} ns **/
+    solve = async (ns, filename, host = 'home') => {
+        const port = ns.getPortHandle(Ports.CONTRACT_PORT);
+        port.clear();
+
+        const input = ns.codingcontract.getData(filename, host);
+        const processedInput = this.processInput(input);
+        ns.run(this.script, 1, JSON.stringify(processedInput), Ports.CONTRACT_PORT);
+
+        while (port.empty()) {
+            await ns.sleep(1);
+        }
+
+        const answer = JSON.parse(port.read());
+        const reward = ns.codingcontract.attempt(answer, filename, host);
+        if (reward === '') {
+            return SolveResult.failure(`Answer: ${answer}, Input: ${JSON.stringify(input)}`);
+        } else {
+            return SolveResult.success(reward);
+        }
+    };
+
+    static findSolver = (title) => {
+        return ContractSolvers.find(s => s.title === title);
+    };
+}
+
+export const ContractSolvers = [
+    new ContractSolver(ContractTypes.FIND_LARGEST_PRIME_FACTOR, `contracts/find-largest-prime-factor.js`),
+    new ContractSolver(ContractTypes.SUBARRAY_WITH_MAXIMUM_SUM, `contracts/subarray-with-maximum-sum.js`),
+    new ContractSolver(ContractTypes.TOTAL_WAYS_TO_SUM_1, `contracts/total-ways-to-sum.js`, (input) => [input, [...Array(input).keys()].filter(a => a > 0)]),
+    new ContractSolver(ContractTypes.TOTAL_WAYS_TO_SUM_2, `contracts/total-ways-to-sum.js`),
+    new ContractSolver(ContractTypes.SPIRALIZE_MATRIX, `contracts/spiralize-matrix.js`),
+    new ContractSolver(ContractTypes.ARRAY_JUMPING_GAME_1, `contracts/array-jumping-game.js`),
+    new ContractSolver(ContractTypes.ARRAY_JUMPING_GAME_2, `contracts/array-jumping-game-ii.js`),
+    new ContractSolver(ContractTypes.MERGE_OVERLAPPING_INTERVALS, `contracts/merge-overlapping-intervals.js`),
+    new ContractSolver(ContractTypes.GENERATE_IP_ADDRESSES, `contracts/generate-ip-addresses.js`),
+    new ContractSolver(ContractTypes.ALGORITHMIC_STOCK_TRADER_1, `contracts/algorithmic-stock-trader.js`, (input) => [1, input]),
+    new ContractSolver(ContractTypes.ALGORITHMIC_STOCK_TRADER_2, `contracts/algorithmic-stock-trader.js`, (input) => [input.length, input]),
+    new ContractSolver(ContractTypes.ALGORITHMIC_STOCK_TRADER_3, `contracts/algorithmic-stock-trader.js`, (input) => [2, input]),
+    new ContractSolver(ContractTypes.ALGORITHMIC_STOCK_TRADER_4, `contracts/algorithmic-stock-trader.js`),
+    new ContractSolver(ContractTypes.MINIMUM_PATH_SUM_IN_A_TRIANGLE, `contracts/minimum-path-sum-in-a-triangle.js`),
+    new ContractSolver(ContractTypes.UNIQUE_PATHS_IN_A_GRID_1, `contracts/unique-paths-in-a-grid.js`, (input) => Array(input[0]).fill(null).map(() => Array(input[1]).fill(0))),
+    new ContractSolver(ContractTypes.UNIQUE_PATHS_IN_A_GRID_2, `contracts/unique-paths-in-a-grid.js`),
+    new ContractSolver(ContractTypes.SHORTEST_PATH_IN_A_GRID, `contracts/shortest-path-in-a-grid.js`),
+    new ContractSolver(ContractTypes.SANITIZE_PARENTHESES_IN_EXPRESSION, `contracts/sanitize-parentheses-in-expression.js`),
+    // new ContractSolver(ContractTypes.FIND_ALL_VALID_MATH_EXPRESSIONS, null),
+    new ContractSolver(ContractTypes.HAMMINGCODES_ENCODE, `contracts/hammingcodes-integer-to-encoded-binary.js`),
+    new ContractSolver(ContractTypes.HAMMINGCODES_DECODE, `contracts/hammingcodes-encoded-binary-to-integer.js`),
+    new ContractSolver(ContractTypes.PROPER_2_COLORING_OF_A_GRAPH, `contracts/proper-2-coloring-of-a-graph.js`),
+    new ContractSolver(ContractTypes.COMPRESSION_1, `contracts/compression-i-rle-compression.js`),
+    new ContractSolver(ContractTypes.COMPRESSION_2, `contracts/compression-ii-lz-decompression.js`),
+    // new ContractSolver(ContractTypes.COMPRESSION_3, null),
+    new ContractSolver(ContractTypes.ENCRYPTION_1, `contracts/encryption-i-caesar-cipher.js`),
+    new ContractSolver(ContractTypes.ENCRYPTION_2, `contracts/encryption-ii-vigenere-cipher.js`),
+];
